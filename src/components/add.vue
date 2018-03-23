486c2e60e8a893bb66f4e2995c8254afd8b510e6
lky1106923807 <1106923807@qq.com>
2018-03-23 09:52:39 +0800

    lky

---
+++ src/components/add.vue
@@ -0,0 +1,40 @@
+<template>
+
+        <div class="edit-content">
+            <form>
+                <div class="form-group">
+                    <label for="name">姓名</label>
+                    <input type="text" class="form-control" id="name" name="name">
+                </div>
+                <div class="form-group">
+                    <label for="age">年龄</label>
+                    <input type="" class="form-control" id="age" name="age">
+                </div>
+                <div class="form-group">
+                    <label for="class">班级</label>
+                    <input type="" class="form-control" id="class" name="class">
+                </div>
+                <div class="form-group">
+                    <label for="items">科目</label>
+                    <input type="" class="form-control" id="items" name="items">
+                </div>
+                <div class="form-group">
+                    <label for="scroe">分数</label>
+                    <input type="" class="form-control" id="scroe" name="scroe">
+                </div>
+                <button type="submit" class="btn btn-default editCon">添加</button>
+            </form>
+        </div>
+
+</template>
+<script>
+    export default {
+        name: 'xxx'
+    }
+</script>
+<style>
+	.edit-content{
+		width: 1200px;
+		margin: 0 auto;
+	}
+</style>
---
+++ src/components/list.vue
@@ -0,0 +1,75 @@
+<template>
+  <div class="list-content">
+  <table class="table table-bordered">
+    <tbody>
+    <tr>
+      <th>id</th>
+      <th>姓名</th>
+      <th>年龄</th>
+      <th>班级</th>
+      <th>学科</th>
+      <th>分数</th>
+      <th>操作</th>
+    </tr>
+    <tr v-for="item in datas">
+      <td>{{item.id}}</td>
+      <td>{{item.name}}</td>
+      <td>{{item.age}}</td>
+      <td>{{item.class}}</td>
+      <td>{{item.items}}</td>
+      <td>{{item.scroe}}</td>
+      <td>
+        <button type="button" class="btn btn-danger">删除</button>
+        <button type="button" class="btn btn-success" @click="edit">编辑</button>
+      </td>
+    </tr>
+    </tbody>
+  </table>
+  </div>
+</template>
+
+<script>
+export default {
+    name:"list",
+    data(){
+        return {
+            datas:[
+                {id:1,name:"小明",age:18,class:1710,items:"语文",scroe:90},
+                {id:2,name:"李四",age:22,class:1710,items:"语文",scroe:80},
+                {id:3,name:"张三",age:20,class:1710,items:"语文",scroe:70},
+            ]
+        }
+    },
+    methods:{
+        edit(){
+            this.$router.push('/edit')
+        }
+    },
+    mounted(){
+        fetch('/api/select').then(function(e){
+            return e.text();
+        }).then((e)=>{
+            console.log(e)
+        })
+    }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+table{
+  width:800px;
+  margin:auto;
+  font-size: 20px;
+  color:black;
+  text-align: center;
+}
+td,th{
+	text-align: center;
+}
+  .btn{
+    height: 30px;
+    font-size:15px;
+    line-height: 10px;
+  }
+</style>
